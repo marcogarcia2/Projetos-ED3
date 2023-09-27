91e095ca56addd73760b3cfe5de81f516074dfc5
Lucas Lima Romero <101420277+luckera@users.noreply.github.com>
2023-09-27 16:30:06 -0300

    Criação de Registros

--- apps/main.c
+++ apps/main.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include <ctype.h>
 
-#include "funcoesFornecidas.h"
+//#include "funcoesFornecidas.h"
 
 #define TAM_REGISTRO 76
 #define TAM_REGISTRO_FIXO 21
 #define LIXO '$'
 
-typedef struct{ // tamanho fixo de 13 bytes? Cabelho
+typedef struct{ // tamanho fixo de 13 bytes - Cabecalho
     char status;
     int proxRRN;
     int nroTecnologias; // indica o nro de tecnologias unicas de origem
@@ -30,30 +30,87 @@ typedef struct{
     StringVariavel tecnologiaDestino;
 } Registro;
 
-// A primeira função é criar a tabela de dados
-// DEpois (?) tem uma funçao que gera um binario aleatorio para checar se criou certo
-// quanndo o campo do registro for vazio, colocar nulo
+// Depois (?) tem uma funçao que gera um binario aleatorio para checar se criou certo
+// quando o campo do registro for vazio, colocar nulo
 
 // comando linux para comparar binario cmp bin1.bin bin2.bin
 
-int main (int argc, char *argv[]){
-
-    //Cabecalho cabecalho = {0, 0, 0, 0};
-    //Registro *registros;    
-
-    // leitura dos dados
-    FILE *arq = fopen("arquivos/dados1.csv","rb");
-    if (arq == NULL){
-        printf("Erro ao abrir o arquivo");
-        return 1;
-    }
+Cabecalho *criaCabecalho(){ // Inicializa o cabecalho
     
+    Cabecalho *c = (Cabecalho *) malloc(sizeof(Cabecalho));
+    c->status = '0';
+    c->proxRRN = 0;
+    c->nroTecnologias = 0;
+    c->nroParesTecnologias = 0;
 
-    fclose(arq);
+    return c;
+}
+
+//void gravaCabecalho(char *nomeArquivoCSV, char *nomeArquivoBIN){
+    // só é executada no final
+// }
+
+Registro *criaRegistro(){ // Aloca-se memória para um registro
+    Registro *r = (Registro *) malloc(sizeof(Registro));
+    
+    return r;
+}
+
+void gravaRegistro(Registro *r, FILE *arqBIN){ // grava o Registro criado no arquivo binário
+    fwrite(&r->removido, sizeof(char), 1, arqBIN);
+    fwrite(&r->grupo, sizeof(int), 1, arqBIN);
+    fwrite(&r->popularidade, sizeof(int), 1, arqBIN);
+    fwrite(&r->peso, sizeof(int), 1, arqBIN);
+    fwrite(&r->tecnologiaOrigem.tamanho, sizeof(int), 1, arqBIN);
+    fwrite(r->tecnologiaOrigem.string, sizeof(char), r->tecnologiaOrigem.tamanho, arqBIN);
+    fwrite(&r->tecnologiaDestino.tamanho, sizeof(int), 1, arqBIN);
+    fwrite(r->tecnologiaDestino.string, sizeof(char), r->tecnologiaDestino.tamanho, arqBIN);
+}
 
 
-    // primeira funcao, criar tabela
+void criaTabela(char *nomeArquivoCSV, char *nomeArquivoBIN){ // Funcionalidade 1
 
+    FILE *arquivoCSV = fopen(nomeArquivoCSV, "r");
+
+    if(arquivoCSV == NULL){
+        printf("Falha no carregamento do Arquivo CSV.\n");
+        return;
+    }
+
+    FILE *arquivoBIN = fopen(nomeArquivoBIN, "wb");
+
+    if(arquivoBIN == NULL){
+        printf("Falha no carregamento do Arquivo Binário.\n");
+        return;
+    }
+
+    //Cabecalho *cabecalho = criaCabecalho();
+
+    Registro *r = criaRegistro();
+
+    // A lógica é criar um loop que irá ler cada linha do arquivo csv
+    while (fscanf(arquivoCSV, "%[^,],%d,%d,%[^,],%d\n", r->tecnologiaOrigem.string, &r->grupo, &r->popularidade, r->tecnologiaDestino.string, &r->peso) != EOF) {
+
+        r->tecnologiaOrigem.tamanho = (int) strlen(r->tecnologiaOrigem.string);
+        r->tecnologiaDestino.tamanho = (int) strlen(r->tecnologiaDestino.string);
+        printf("%d\n", r->tecnologiaOrigem.tamanho);
+        printf("%d\n", r->tecnologiaDestino.tamanho);
+
+        // Coloca os valores lidos no arquivo binário
+        gravaRegistro(r, arquivoBIN);
+    }
+
+    //gravaCabecalho(nomeArquivoCSV, nomeArquivoBIN);
+    
+    fclose(arquivoCSV);
+    fclose(arquivoBIN);
+
+}
+
+int main (){
+   
+// primeira funcao, criar tabela
+    criaTabela("tecnologia.csv","tecnologia.bin");
 
     return 0;
 }
