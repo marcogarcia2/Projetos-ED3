4b596a846a159ea34bcca0684b674fbbdf0e8cbb
marcogarcia2 <mmgaspargarcia@gmail.com>
2023-11-24 19:09:29 -0300

    Testando

--- Trabalho Prático 1/include/funcoesIndice.h
+++ Trabalho Prático 1/include/funcoesIndice.h
@@ -12,24 +12,15 @@
 #include "arvoreB.h"
 
 // Função que cria um Cabeçalho de Árvore, inserido no início do arquivo binário de índice
-CabecalhoArv *criaCabecalhoArv(void);
+CabecalhoIndice *criaCabecalhoIndice(void);
 
 // Função que grava o Cabeçalho de Árvore no início do arquivo
-void gravaCabecalhoArv(CabecalhoArv *cArv, FILE *arquivoIND);
-
-// Função que cria um Cabeçalho de Nó, inserido antes de cada nó
-CabecalhoNo *criaCabecalhoNo(void);
-
-// Função que grava o Cabeçalho  
-void gravaCabecalhoNo(CabecalhoNo *cNo, FILE *arquivoIND);
+void gravaCabecalhoIndice(CabecalhoIndice *cIndice, FILE *arquivoIND);
 
 // Função que grava um nó (ou página) no arquivo binário de índice
 void gravaNo(No *no, FILE *arquivoIND);
 
-// liberaNo está na implementação do TAD de árvore B
-// não pensei nas outras funções ainda
 
-// 
 
 
 #endif
