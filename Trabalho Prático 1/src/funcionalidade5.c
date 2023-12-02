e117782de57e9e0a820e1a2730159120a4467ea4
Lucas Lima Romero <lucasromero1702@gmail.com>
2023-12-02 16:37:40 -0300

    Comentando: Funcionalidade 5, funcoesIndice e funcoesCriadas .c e .h

--- Trabalho Prático 1/src/funcionalidade5.c
+++ Trabalho Prático 1/src/funcionalidade5.c
@@ -3,11 +3,6 @@
 *   Marco Antonio Gaspar Garcia - 11833581
 */
 
-/*  
-*  Fazer uma função que lê um arquivo binário definido anteriormente no trabalho introdutório, e a
-*  partir disso criar um novo arquivo binário (de índice), que representa uma estrutura de árvore B.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
